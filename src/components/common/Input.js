e3839377e0936b426806a27000622459b83765fc
ovyny <ovyny@mac-mini-de-ovyny.home>
2018-02-10 22:13:25 +0100

    android text no underline

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -8,6 +8,7 @@ const Input = ({ label, value, onChangeText, placeholder, secureTextEntry }) => 
     <View style={containerStyle}>
       <Text style={labelStyle}>{label}</Text>
       <TextInput
+        underlineColorAndroid={'transparent'}
         secureTextEntry={secureTextEntry}
         placeholder={placeholder}
         autoCorrect={false}
